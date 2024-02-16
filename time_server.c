7462a7882e704bad023177bc56a5cb8662f6c59c
miloud <miloamg@gmail.com>
2024-02-16 03:52:12 +0100

    added server

---
+++ time_server.c
@@ -0,0 +1,128 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+#endif
+
+int main() {
+#if defined(_WIN32)
+  WSADATA d;
+  if (WSAStartup(MAKEWORD(2, 2), &d)) {
+    fprintf(stderr, "Failed to initialize.\n");
+    return 1;
+  }
+#endif
+
+  printf("Configuring local address...\n");
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  struct addrinfo *bind_address;
+  int r = getaddrinfo(0, "8080", &hints, &bind_address);
+
+  printf("Creating the socket\n");
+  SOCKET socket_listen;
+  socket_listen = socket(bind_address->ai_family, bind_address->ai_socktype,
+                         bind_address->ai_protocol);
+
+  if (!ISVALIDSOCKET(socket_listen)) {
+    fprintf(stderr, "failed to create socket %d\n", GETSOCKETERRNO());
+    return 1;
+  }
+
+  int binded;
+  printf("Binding sochet with address\n");
+  binded = bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen);
+  if (binded) {
+    printf("Failed to bind socket %d\n", GETSOCKETERRNO());
+    return 1;
+  }
+  freeaddrinfo(bind_address);
+
+  printf("Listening...\n");
+  if (listen(socket_listen, 10) < 0) {
+    fprintf(stderr, "Failed listening %d\n", GETSOCKETERRNO());
+    return 1;
+  }
+
+  printf("Waiting for connection\n");
+  struct sockaddr_storage client_address;
+  socklen_t client_len = sizeof(client_address);
+  SOCKET socket_client =
+      accept(socket_listen, (struct sockaddr *)&client_address, &client_len);
+
+  if (!ISVALIDSOCKET(socket_client)) {
+    fprintf(stderr, "accept() failed %d\n", GETSOCKETERRNO());
+    return 1;
+  }
+
+  printf("Client is connected\n");
+  char address_buffer[100];
+  getnameinfo((struct sockaddr *)&client_address, client_len, address_buffer,
+              sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
+  printf("%s\n", address_buffer);
+
+  printf("Receaving request\n");
+  char request[1024];
+  int recieved_bytes = recv(socket_client, request, 1024, 0);
+  printf("recieved %d bytes\n", recieved_bytes);
+
+  printf("Sending response\n");
+  const char *response = "HTTP/1.1 200 OK\r\n"
+                         "Connection: close\r\n"
+                         "Content-Type: text/plain\r\n\r\n"
+                         "Local time is: ";
+  int bytes_sent = send(socket_client, response, strlen(response), 0);
+  printf("sent %d of %d of bytes\n", bytes_sent, (int)strlen(request));
+
+  time_t timer;
+  time(&timer);
+  char *time_msg = ctime(&timer);
+  bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
+  printf("sent %d of %d\n", bytes_sent, (int)strlen(time_msg));
+  // printf("%s\n", time_msg);
+
+  printf("Closing connection\n");
+  CLOSESOCKET(socket_client);
+
+  printf("Closing listening socket\n");
+  CLOSESOCKET(socket_listen);
+
+#if defined(_WIN32)
+  WSACleanup();
+#endif
+
+  printf("Finnish\n");
+
+  return 0;
+}
